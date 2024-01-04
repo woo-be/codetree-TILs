c0ec3ca7e4037291efc49c622f5c28c96f158443
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-04 20:48:01 +0900

    Title: 실수 입력받아 계산 2, Time: 156ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240104/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        System.out.printf("%.2f", a + 1.5);
+    }
+}
