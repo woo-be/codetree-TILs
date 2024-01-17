98910be945aa8282e1ee45edbfe9f437eefdc900
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-17 20:38:36 +0900

    Title: 숫자의 조건 여부, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240117/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a >= 113) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
