5aec416b26f2a7cfcb7cbbea3821024dc97b7375
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-18 12:27:47 +0900

    Title: 비교에 따른 연산, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a > b) {
+            System.out.println(a * b);
+        } else {
+            System.out.println(b / a);
+        }
+    }
+}
