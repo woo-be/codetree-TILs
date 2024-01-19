e2fad0bcfd4f597ee6a928bf9a5384de05abd5f5
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-19 17:29:10 +0900

    Title: 물의 상태, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240119/물의 상태/state-of-water.java
+++ 240119/물의 상태/state-of-water.java
@@ -6,7 +6,7 @@ public class Main {
         int temperature = in.nextInt();
         if (temperature < 0) {
             System.out.println("ice");
-        } else if (temperature > 100) {
+        } else if (temperature >= 100) {
             System.out.println("vapor");
         } else {
             System.out.println("water");
