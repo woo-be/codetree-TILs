3ebe542c31ea961ec701d984d8f8a49a640256d9
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-20 16:10:06 +0900

    Title: 등급 매기기, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240120/등급 매기기/ranking.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n >= 90) {
+            System.out.println("A");
+        } else if (n >= 80) {
+            System.out.println("B");
+        } else if (n >= 70) {
+            System.out.println("C");
+        } else if (n >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
