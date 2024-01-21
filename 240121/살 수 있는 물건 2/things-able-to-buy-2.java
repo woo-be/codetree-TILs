8bd51edd0654d75ff1fe28bf6f4d8e5afbd4e0da
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-21 12:42:24 +0900

    Title: 살 수 있는 물건 2, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240121/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if (n >= 3000) {
+            System.out.println("book");
+        } else if (n >= 1000) {
+            System.out.println("mask");
+        } else if (n >= 500) {
+            System.out.println("pen");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
