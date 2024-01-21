9b59f86ce9cd64d1af264e1efc4e4c6b1d9ea396
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-21 12:47:54 +0900

    Title: 최대 2번의 연산, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240121/최대 2번의 연산/up-to-2-calculations.java
+++ 240121/최대 2번의 연산/up-to-2-calculations.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         if (a % 2 == 0) {
-            a = a / 2;
-            if ( a % 2 == 1) {
-                a = a + 1;
-                a = a / 2;
-            }
+            a = a / 2;   
+        }
+        if (a % 2 == 1) {
+           a = a + 1;
+           a = a / 2;
         }
         System.out.println(a);
     }
