e049efc3babc98cd0e0b919c3863f27e68ea406b
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-22 10:00:44 +0900

    Title: 두 번의 연산, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240122/두 번의 연산/two-operations.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a % 2 == 1) {
+            a += 3;
+        }
+        if (a % 3 == 0) {
+            System.out.println(a / 3);
+        }
+    }
+}
