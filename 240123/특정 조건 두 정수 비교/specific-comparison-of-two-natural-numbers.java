f86bfb139cca4f45688cdbe052eb3beac6d0e278
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-23 11:25:09 +0900

    Title: 특정 조건 두 정수 비교, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240123/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        if (a < b) {
+            System.out.println("1 0");
+        } else if (a == b) {
+         System.out.println("0 1");   
+        }
+    }
+}
