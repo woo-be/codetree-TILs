6b24b263294c148c5794dcb28ef53c1a2ec3e14d
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-24 09:34:49 +0900

    Title: 4번의 크기 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240124/4번의 크기 비교/4-time-comparison.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int e = in.nextInt();
+        if (a > b) {
+            System.out,println("1");
+        } else {
+            System.out,println("0");
+        }
+        if (a > c) {
+            System.out,println("1");
+        } else {
+            System.out,println("0");
+        }
+        if (a > d) {
+            System.out,println("1");
+        } else {
+            System.out,println("0");
+        }
+        if (a > e) {
+            System.out,println("1");
+        } else {
+            System.out,println("0");
+        }
+    }
+}
