19a2a4cbef60b4e3e7608919b6e776c5a5d6ecc9
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-25 09:56:45 +0900

    Title: 범위 안의 수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240125/범위 안의 수/number-in-range.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        if (a >= 10 && a <= 20) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
