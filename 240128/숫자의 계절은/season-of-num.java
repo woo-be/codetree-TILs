2ab9eda0b47b32755e3c0814d2744d6671f1d100
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-28 12:04:33 +0900

    Title: 숫자의 계절은, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240128/숫자의 계절은/season-of-num.java
+++ 240128/숫자의 계절은/season-of-num.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int m = in.nextInt();
-        if (m >= 3) {
+        if (m >= 3 && m <=5) {
             System.out.println("Spring");
-        } else if (m >= 6) {
+        } else if (m <= 8) {
             System.out.println("Summer");
-        } else if (m >= 9) {
+        } else if (m <= 11>) {
             System.out.println("Fall");
         } else {
             System.out.println("Winter");
