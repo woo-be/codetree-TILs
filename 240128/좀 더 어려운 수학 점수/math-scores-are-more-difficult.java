193dd6b556b2785d20dae24b96d9f5c967566c54
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-28 12:13:28 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240128/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int aMath = in.netxInt();
+        int aEngilsh = in.netxInt();
+        int bMath = in.netxInt();
+        int bEnglish = in.netxInt();
+        if (aMath > bMath) {
+            System.out.println("A")
+        } else if (aMath < bMath) {
+            System.out.println("B")
+        } else {
+            if (aEngilsh > bEnglish) {
+                System.out.println("A")
+            } else {
+                System.out.println("B")
+            }
+        }
+    }
+}
