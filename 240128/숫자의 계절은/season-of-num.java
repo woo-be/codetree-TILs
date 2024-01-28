cbb40a9ab44139037f475a24337684e8d7a81d6f
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-28 12:06:26 +0900

    Title: 숫자의 계절은, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240128/숫자의 계절은/season-of-num.java
+++ 240128/숫자의 계절은/season-of-num.java
@@ -6,9 +6,9 @@ public class Main {
         int m = in.nextInt();
         if (m >= 3 && m <=5) {
             System.out.println("Spring");
-        } else if (m <= 8) {
+        } else if (m >= 6 && m <= 8) {
             System.out.println("Summer");
-        } else if (m <= 11) {
+        } else if (m >= 9 && m <= 11) {
             System.out.println("Fall");
         } else {
             System.out.println("Winter");
