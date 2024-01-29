353aa7b81e2dc3da3274b48d3538a2e2f7762b4c
woo-be <150896971+woo-be@users.noreply.github.com>
2024-01-29 17:57:18 +0900

    Title: 윤년인가, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/윤년인가/is-leap-year.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int y = in.nextInt();
+        if (y % 4 == 0) {
+            if (y % 100 == 0) {
+                if (y % 400 == 0) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            } else {
+                System.out.println("true");
+            }
+        } else {
+            System.out.println("false");
+        }
+    }
+}
