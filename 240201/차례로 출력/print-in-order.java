0ded9cd941fb3153c16f553fa0ef239bb5899627
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-01 09:42:16 +0900

    Title: 차례로 출력, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240201/차례로 출력/print-in-order.java
@@ -0,0 +1,10 @@
+// import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner in = new Scanner(System.in);
+        for (int i = 5; i <= 17; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
