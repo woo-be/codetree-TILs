812800dbbefc9d85b320d2b68e74833ad9ed2dfa
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-04 15:07:13 +0900

    Title: 감소시키며 출력하기, Time: 114ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240204/감소시키며 출력하기/decrease-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        for(int i = 17; i > 4; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
