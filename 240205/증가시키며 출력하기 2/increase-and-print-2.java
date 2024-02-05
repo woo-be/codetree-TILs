3a763e2a240d241f3b98bd784f0da1d2ab0bf72a
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-05 15:05:30 +0900

    Title: 증가시키며 출력하기 2, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 10;
+        while (i < 27) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
