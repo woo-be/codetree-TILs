5a5dda0ae9748b959c6c2f5dfede73a53226603f
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-08 09:25:02 +0900

    Title: 문자 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240208/문자 출력하기/print-text.java
+++ 240208/문자 출력하기/print-text.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String str = in.next();
         for (int i = 0; i < 8; i++) {
-            System.out.print("G");
+            System.out.print(str);
         }
     }
 }
