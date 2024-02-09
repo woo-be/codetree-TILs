136bd4b5c35f68c28a2137662443ca8f9a4543eb
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-09 15:12:33 +0900

    Title: a/b 출력, Time: 212ms, Memory: 10MB, Status: Passed - CodeTree

--- 240209/a/b 출력/a-divide-b.java
+++ 240209/a/b 출력/a-divide-b.java
@@ -3,8 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double a = in.nextInt();
-        double b = in.nextInt();
-        System.out.printf("%.20f", a / b);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int rest = a % b;
+        int temp = 0;
+        System.out.print(a / b + ".");
+        for (int i = 0; i < 20; i++) {
+            temp = rest * 10;
+            System.out.print(temp / b);
+            rest = temp % b;
+        }
     }
 }
