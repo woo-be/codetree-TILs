8ab15749a523397f8f0a6970b12d8fc6390563d7
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-16 09:36:20 +0900

    Title: 완전수 판별기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (n == sum) {
+            System.out.print("P");
+        } else {
+            System.out.print("N");
+        }
+    }
+}
