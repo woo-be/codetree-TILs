bee5b7490ea2f24a6ab6ddc64b2c60a5f3235c9e
woo-be <150896971+woo-be@users.noreply.github.com>
2024-02-23 09:31:44 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            int i = in.nextInt();
+            if (i >= 30) {
+                break;
+            }
+            sum += i;
+            count++;
+        }
+        System.out.printf("%.2f", (double) sum / count);
+    }
+}
