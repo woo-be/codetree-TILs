544074917cc705c58546d1bed5f9b0863a7d918c
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-05 14:05:06 +0900

    Title: 별표 출력하기 4, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240305/별표 출력하기 4/print-star-4.java
+++ 240305/별표 출력하기 4/print-star-4.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-i; j++) {
+        int cnt = n;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < cnt; j++) {
                 System.out.print("* ");
             }
             System.out.println();
-        }
 
-        for (int i = n-2; i >= 0; i--) {
-            for (int j = 0; j < n-i; j++) {
-                System.out.print("* ");
+            if (i >= 4) {
+                cnt++;
+            } else {
+                cnt--;
             }
-            System.out.println();
         }
     }
 }
