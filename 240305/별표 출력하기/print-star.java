ccf4b9c40a55df6658e570f5a9fcf22b62c8333f
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-05 12:22:46 +0900

    Title: 별표 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/별표 출력하기/print-star.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
