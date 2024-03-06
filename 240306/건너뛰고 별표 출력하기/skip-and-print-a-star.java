46fbec652843ff4888d1af60e1568a3f87f2a4ac
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-06 09:44:24 +0900

    Title: 건너뛰고 별표 출력하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240306/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int cnt = 1;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < cnt; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+
+            if (i < n - 1) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+    }
+}
