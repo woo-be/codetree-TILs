e3b834f32960ec78f503f47bd63e6e00393e1752
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-08 14:46:08 +0900

    Title: 별표 출력하기 6, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

--- 240308/별표 출력하기 6/print-star-6.java
+++ 240308/별표 출력하기 6/print-star-6.java
@@ -4,34 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int space = 0;
-        int star = 2 * n - 1;
-        for (int i = 0; i < 2 * n - 1; i++) {
+        for (int i = 0; i < n; i++) {
             // 공백
-            for(int j = 0; j < space; j++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("  ");
             }
             // 별
-            for (int k = 0; k < star; k++) {
+            for (int k = 0; k < (2 * n) - (2 * i) - 1; k++) {
                 System.out.print("* ");
             }
             System.out.println();
-
-            if (i < n-1) {
-                space++;
-                star -= 2;
-            } else {
-                space--;
-                star += 2;
-            }
         }
+        for (int i = 0; i < n - 1; i++) {
+            // 공백
+            for (int j = 0; j < n - 2 - i; j++) {
+                System.out.print("  ");
+            }
+            // 별
+            for (int k = 0; k < 3 + (2 * i); k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
 
-// i             v *
-// 0 * * * * *   0 5
-// 1 v * * *     1 3
-// 2 v v *       2 1
-// 3 v * * *     1 3
-// 4 * * * * *   0 5
-//   v =
+// n = 4
+// i                v *
+// 0 * * * * * * *  0 7
+// 1 v * * * * *    1 5
+// 2 v v * * *      2 3
+// 3 v v v *        3 1
+// v = i, * = (2 * n) - (2 * i) - 1
+// -----------------------
+// 0 v v * * *      2 3 
+// 1 v * * * * *    1 5 
+// 2 * * * * * * *  0 7 
+// v = n - 2 - i, * = 3 + 2 * i
