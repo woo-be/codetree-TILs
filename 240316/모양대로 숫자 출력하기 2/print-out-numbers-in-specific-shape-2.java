8c68cbe04591e2be4cf02a20b4b39adbf1caf7fe
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-16 17:47:59 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240316/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int cnt = 2;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(cnt + " ");
+                cnt = cnt + 2;
+                if (cnt == 10) 
+                    cnt = 2;
+            }
+            System.out.println();
+        }
+    }
+}
