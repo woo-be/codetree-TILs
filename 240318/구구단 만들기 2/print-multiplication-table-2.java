748ba5925b922af8c71051fb6450a86760b345a0
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-18 10:36:06 +0900

    Title: 구구단 만들기 2, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/구구단 만들기 2/print-multiplication-table-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        for (int i = 2; i < 9; i++) {
+            if (i % 2 == 0) {
+                for (int j = b; j > 1; j--) {
+                    System.out.printf("%d * %d = %d", j, i, j * i);
+                    if (j != 2) {
+                        System.out.print(" / ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
