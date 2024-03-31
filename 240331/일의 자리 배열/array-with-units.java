e2cc20f1d89803f0b74bab89582f348c251be4bc
woo-be <150896971+woo-be@users.noreply.github.com>
2024-03-31 15:45:29 +0900

    Title: 일의 자리 배열, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240331/일의 자리 배열/array-with-units.java
+++ 240331/일의 자리 배열/array-with-units.java
@@ -11,7 +11,7 @@ public class Main {
         }
 
         for (int i = 0; i < 10; i++) {
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] % 10 + " ");
         }
     }
 }
