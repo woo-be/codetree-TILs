83c1532ab4e0827d135df3809c9941218fe73144
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-01 11:27:04 +0900

    Title: 전항의 두 배, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240401/전항의 두 배/twice-the-previous.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[11];
+        arr[0] = in.nextInt();
+        arr[1] = in.nextInt();
+        for (int i = 2; i < 10; i++) {
+            arr[i] = arr[i-1] + 2 * arr[i-2];
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
