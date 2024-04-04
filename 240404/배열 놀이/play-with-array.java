35aa79ae486b813c0f02d19fb4e612dceff816fb
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-04 11:23:52 +0900

    Title: 배열 놀이, Time: 206ms, Memory: 11MB, Status: Passed - CodeTree

--- 240404/배열 놀이/play-with-array.java
+++ 240404/배열 놀이/play-with-array.java
@@ -6,22 +6,24 @@ public class Main {
         int n = in.nextInt();
         int q = in.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // 배열 초기화
             arr[i] = in.nextInt();
         }
-        for (int i = 0; i < q; i++) {
+        for (int i = 0; i < q; i++) { // 질의문 실행
             int q1 = in.nextInt();
             int q2 = in.nextInt();
             if (q1 == 1) {
-                System.out.println(arr[q1 - 1]);
+                // System.out.print("q1 = " + q1 + ", q2 = " + q2 + " ");
+                System.out.println(arr[q2 - 1]);
             } else if (q1 == 2) {
                 int index = 0;
                 for (int j = 0; j < n; j++) {
                     if (arr[j] == q2) {
-                        index = j;
+                        index = j + 1;
+                        break;
                     }
                 }
-                System.out.println(index + 1);
+                System.out.println(index);
             } else if (q1 == 3) {
                 int q3 = in.nextInt();
                 for (int j = q2 -1; j <= q3 - 1; j++) {
