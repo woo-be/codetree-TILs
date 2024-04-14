f8e49ce0db7eacf71d09149ccc9d8da8bf98f326
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-14 14:08:51 +0900

    Title: 특정 원소들의 합, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240414/특정 원소들의 합/sum-of-specific-elements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[][] arr = new int[4][4];
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.print(sum);
+    }
+}
