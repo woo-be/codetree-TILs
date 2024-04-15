5f68deb089c56779c7020247275c46c20f137b36
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-15 09:54:51 +0900

    Title: 숫자 직사각형, Time: 249ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240415/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[n][m];
+        int num = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = num++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
