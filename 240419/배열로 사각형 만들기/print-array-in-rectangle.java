8c90fce37c5cd164488dee7a00d3670f7f75b7c1
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-19 09:34:29 +0900

    Title: 배열로 사각형 만들기, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240419/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < 5; i++) {
+            arr[0][i] = 1;
+            arr[i][0] = 1;
+        }
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                arr[i][j] = arr[i-1][j] + arr[i][j-1];
+            }
+        }
+        
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
