df0f9b340bdde03ee5eaf7f6c26252c20996b33f
woo-be <150896971+woo-be@users.noreply.github.com>
2024-04-26 09:41:20 +0900

    Title: 격자 모양 출력하기, Time: 203ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240426/격자 모양 출력하기/print-grid-shape.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < m; i++) {
+            int x = in.nextInt();
+            int y = in.nextInt();
+            arr[x-1][y-1] = x * y;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
