a6f077427ab12f1ff0a2748b3e0a4394b292137f
woo-be <150896971+woo-be@users.noreply.github.com>
2024-05-30 15:49:21 +0900

    Title: 부분문자열 위치 구하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240530/부분문자열 위치 구하기/find-location-of-substring.java
+++ 240530/부분문자열 위치 구하기/find-location-of-substring.java
@@ -5,9 +5,15 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String inStr = in.next();
         String tarStr = in.next();
-        // int inStrLength = inStr.length();
-        // int tarStrLength = tarStr.length();
-        // int startIdx = -1;
-        System.out.print(inStr.indexOf(tarStr));
+        int inStrLength = inStr.length();
+        int tarStrLength = tarStr.length();
+        int startIdx = -1;
+        for (int i = 0; i < inStrLength - tarStrLength + 1; i++) {
+            if (inStr.substring(i, i + tarStrLength).equals(tarStr)) {
+                startIdx = i;
+                break;
+            }
+        }
+        System.out.print(startIdx);
     }
 }
