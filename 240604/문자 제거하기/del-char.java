907f06fb7f98a9a1c4ef2c516c823a2b13eb3cd7
woo-be <150896971+woo-be@users.noreply.github.com>
2024-06-04 22:02:24 +0900

    Title: 문자 제거하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/문자 제거하기/del-char.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    Scanner in = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String str = in.next();
+    int len = str.length();
+    for (int i = 0; i < len - 1; i++) {
+        int index = in.nextInt();
+        if (index >= str.length()) {
+            index = str.length() - 1;
+        }
+        str = str.substring(0, index) + str.substring(index + 1);
+        bw.write(str + "\n");
+    }
+
+    bw.flush();
+    br.close();
+    bw.close();
+  }
+}
